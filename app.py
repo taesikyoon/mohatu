1783dfc9abdbc6572a5559f6b4cf158fa055d1fa
Taesik yoon <taesikyoon97@naver.com>
2022-06-21 01:54:22 +0900

    2020-06-21 01:54

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
-import response
-from flask import Flask
-from flask import render_template
-from flask import request, make_response
-app = Flask(__name__)
-#쿠키와 세션
-# 회원가입하면 쿠키를 서버에서 줌
-# 회원가입정보 디비에 연동해서 넣기
-
+from flask import Flask, render_template, request, session, url_for,redirect
 from pymongo import MongoClient
+# 데이타임? 시간을 만들어준다고해야하나 아무튼 시간함수가있따
+# 시간함수는 세션의 유통기한을 정할때 쓸것이다
 client = MongoClient('mongodb+srv://taesikyoon97:louis17467@cluster0.ncjxm.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-ID = 'test'
-PW = 'taesik'
-@app.route("/")
+
+app = Flask(__name__)
+
+# 세션 암호화
+app.secret_key = "ptmq6dhs2#$%!l"
+
+#메인화면
+
+@app.route("/",methods=['GET'])
 def index():
-    return  render_template('index.html')
+    if "user" in session : return render_template('index.html',login=True)
+    else : return redirect(url_for("login"))
 
-
-@app.route("/login", methods=['GET'])
+#로그인 화면
+@app.route("/login", methods=['POST','GET']) # POST와 GET 을 내주니 오류 없이 됐다...이유는 알수없음..
 def login():
-    user_id = request.args.get('userID')
-    print(user_id)
-    return render_template('login.html')
+    if request.method == "POST":
+      # test= request.form.get('id_give', False)
+        user = request.form['id_give']
+        session['user'] = user
+        return redirect(url_for('index'))
+    else :
+        if "user" in session:
+            return redirect(url_for('index'))
+# 회원가입
+app.route("/join_member", methods=["POST"])
+def join_member():
+    #회원가입 필요[ 이름,나이,성별,이메일,주소,전화번호,아이디,비밀번호,비밀번호 재확인,아이디 중복확인버튼]
+    # 간단하게는 이름 아이디 비밀번호 필요함
+    pass
+
+#test shop,newspage,webtoon >> 등으로 세션 유지 확인
+app.route('/user')
+def user():
+    return "well come " + user
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
